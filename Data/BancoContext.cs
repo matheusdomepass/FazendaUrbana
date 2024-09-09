ef5c90a6699dde051d10db085a9c2691be4fc9d9
Matheus Passatuto <matheus.passatuto@bkbank.com.br>
2024-09-09 11:31:22 -0300

    Adicionar arquivos de projeto.

---
+++ Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using ControleContratos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleContratos.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
